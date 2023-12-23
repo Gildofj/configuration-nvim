5041fa05009e190594e4e3b39566d6e8ace72b99
Gildo Junior <gildo.junior@omie.com.br>
2023-12-23 15:55:13 -0300

    Ajusta plugins

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -26,14 +26,38 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      { "antosha417/nvim-lsp-file-operations", config = true },
+    },
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.tsserver.setup({ capabilities = capabilities })
+      lspconfig.csharp_ls.setup({ capabilities = capabilities })
+      lspconfig.cssmodules_ls.setup({ capabilities = capabilities })
+      lspconfig.eslint.setup({ capabilities = capabilities })
+      lspconfig.html.setup({ capabilities = capabilities })
+      lspconfig.jsonls.setup({ capabilities = capabilities })
+      lspconfig.jdtls.setup({ capabilities = capabilities })
+      lspconfig.kotlin_language_server.setup({ capabilities = capabilities })
+      lspconfig.tailwindcss.setup({ capabilities = capabilities })
+
+      vim.keymap.set("n", "gR", ":Telescope lsp_references<CR>", {})
+      vim.keymap.set("n", "gf", ":Lspsaga lsp_finder<CR>", {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gd", ":Telescope lsp_definitions<CR>", {})
+      vim.keymap.set("n", "gi", ":Telescope lsp_implementations<CR>", {})
+      vim.keymap.set("n", "gt", ":Telescope lsp_type_definitions<CR>", {})
       vim.keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, {})
+      vim.keymap.set({ "n", "v" }, "<leader>r", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<leader>D", ":Telescope diagnostics bufnr=0<CR>", {})
+      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {})
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", {})
     end
-  }
+  },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,12 +3,33 @@ return {
     "nvim-telescope/telescope.nvim", 
     tag = "0.1.x", 
     dependencies = {
-      "nvim-lua/plenary.nvim"
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     }, 
     config = function()
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      
+      -- require("telescope").setup({
+      --   defaults = {
+      --     path_display = { "truncate " },
+      --     mappings = {
+      --       i = {
+      --         ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+      --         ["<C-j>"] = actions.move_selection_next, -- move to next result
+      --         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, 
+      --       }
+      --     }
+      --   }
+      -- })
+      -- require("telescope").load_extension("fzf")
+      --
+      -- keymap.set("n", "<leader>ff", ":Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+      -- keymap.set("n", "<leader>fr", ":Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+      -- keymap.set("n", "<leader>fs", ":Telescope live_grep<cr>", { desc = "Find string in cwd" })
+      -- keymap.set("n", "<leader>fc", ":Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     end
   },
   {
@@ -25,8 +46,4 @@ return {
       require("telescope").load_extension("ui-select")
     end
   },
-  {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    run = "make"
-  }
 }
