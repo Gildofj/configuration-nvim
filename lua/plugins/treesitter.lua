eb04bc2e534904224c0beb6da29b0b011d23f2aa
Gildo Junior <gildo.junior@omie.com.br>
2023-12-24 19:06:40 -0300

    Adicionar e ajusta plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,42 @@
-return { 
-  "nvim-treesitter/nvim-treesitter", 
+return {
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  dependencies = {
+    -- "nvim-treesitter/nvim-treesitter-textobjects",
+    "windwp/nvim-ts-autotag",
+  },
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
       ensure_installed = {
-        "lua", 
-        "javascript", 
-        "typescript", 
-        "vimdoc", 
-        "query", 
-        --    "angular", --Experimental 
-        "bash", 
-        "c_sharp", 
-        "css", 
+        "lua",
+        "javascript",
+        "typescript",
+        "vimdoc",
+        "query",
+        --    "angular", --Experimental
+        "bash",
+        "c_sharp",
+        "css",
         "html",
         "java",
         "kotlin",
         "sql"
       },
       highlight = { enable = true },
-      indent = { enable = true }
+      indent = { enable = true },
+      autotag = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
     })
+    -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
+    require('ts_context_commentstring').setup {}
   end
 }
