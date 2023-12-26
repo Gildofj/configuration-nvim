28a65a62d8d0d0a51b0b9f94fd09b6fbfdcc3dbc
Gildo Junior <gildo.junior@omie.com.br>
2023-12-26 11:04:26 -0300

    fix telescope fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,27 +10,38 @@ return {
 		config = function()
 			local opts = { noremap = true, silent = true }
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", builtin.find_files, opts)
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, opts)
+			local actions = require("telescope.actions")
 
-			-- require("telescope").setup({
-			--   defaults = {
-			--     path_display = { "truncate " },
-			--     mappings = {
-			--       i = {
-			--         ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-			--         ["<C-j>"] = actions.move_selection_next, -- move to next result
-			--         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-			--       }
-			--     }
-			--   }
-			-- })
-			-- require("telescope").load_extension("fzf")
-			--
-			-- keymap.set("n", "<leader>ff", ":Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-			-- keymap.set("n", "<leader>fr", ":Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-			-- keymap.set("n", "<leader>fs", ":Telescope live_grep<cr>", { desc = "Find string in cwd" })
-			-- keymap.set("n", "<leader>fc", ":Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+			require("telescope").setup({
+				defaults = {
+					path_display = { "truncate " },
+					mappings = {
+						i = {
+							["<C-k>"] = actions.move_selection_previous, -- move to prev result
+							["<C-j>"] = actions.move_selection_next, -- move to next result
+							["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						},
+					},
+				},
+				extensions = {
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "smart_case",
+					},
+				},
+			})
+			require("telescope").load_extension("fzf")
+
+			opts.desc = "Fuzzy find files"
+			vim.keymap.set("n", "<C-p>", builtin.find_files, opts)
+			opts.desc = "Find recent files"
+			vim.keymap.set("n", "<leader>fr", builtin.oldfiles, opts)
+			opts.desc = "Find string"
+			vim.keymap.set("n", "<leader>fs", builtin.live_grep, opts)
+			opts.desc = "Find string under cursor"
+			vim.keymap.set("n", "<leader>fc", builtin.grep_string, opts)
 		end,
 	},
 	{
