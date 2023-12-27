b7c369f5e5b181b4cb4985f0b6e8d3ac9c6dc88b
Gildo Junior <gildo.junior@omie.com.br>
2023-12-26 23:06:21 -0300

    Reorganiza pastas

--- lua/core/vim-options.lua
+++ lua/core/vim-options.lua
@@ -4,3 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.opt.termguicolors = true
 vim.g.mapleader = " "
+
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
