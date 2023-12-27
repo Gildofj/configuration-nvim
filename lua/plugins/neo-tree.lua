fbb5f02dcb7d51457aadc447a19deb83e7fe706e
Gildo Junior <gildo.junior@omie.com.br>
2023-12-27 20:11:38 -0300

    change neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,16 +5,16 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-      },
-    },
+    "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    })
     vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left<CR>", {})
   end,
 }
