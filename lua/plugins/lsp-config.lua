2c0bec82d3170f4fe33a6f3bbcc3af9f434b777d
Gildo Junior <gildo.junior@omie.com.br>
2023-12-27 16:49:15 -0300

    ajusta lsp-config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -40,7 +40,7 @@ return {
 				ensure_installed = {
 					"prettier", -- prettier formatter
 					"stylua", -- lua formatter
-					"eslint_d", -- js linter
+					"eslint", -- js linter
 				},
 			})
 		end,
