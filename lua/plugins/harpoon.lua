ec780be756a3775c97912cc00f7a696246c057e7
Gildo Junior <gildo.junior@omie.com.br>
2023-12-28 13:44:27 -0300

    list harpoon marks in telescope

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -27,12 +27,7 @@ return {
       "<cmd>lua require('harpoon.ui').nav_next()<cr>",
       { desc = "Go to next harpoon mark" }
     )
-    vim.keymap.set(
-      "n",
-      "<leader>ha",
-      "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
-      { desc = "Show harpoon marks" }
-    )
+    vim.keymap.set("n", "<leader>ha", "<cmd>Telescope harpoon marks<cr>", { desc = "Show harpoon marks" })
     vim.keymap.set("n", "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", { desc = "Nav to file 1" })
     vim.keymap.set("n", "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", { desc = "Nav to file 2" })
     vim.keymap.set("n", "<leader>h3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", { desc = "Nav to file 3" })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,12 +6,15 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      "nvim-telescope/telescope-ui-select.nvim",
+      "ThePrimeagen/harpoon",
     },
     config = function()
       local opts = { noremap = true, silent = true }
       local builtin = require("telescope.builtin")
       local actions = require("telescope.actions")
 
+      -- set telescope config and set extensions
       require("telescope").setup({
         defaults = {
           path_display = { "truncate " },
@@ -30,10 +33,18 @@ return {
             override_file_sorter = true,
             case_mode = "smart_case",
           },
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
         },
       })
-      require("telescope").load_extension("fzf")
 
+      -- load extensions
+      require("telescope").load_extension("fzf")
+      require("telescope").load_extension("ui-select")
+      require("telescope").load_extension("harpoon")
+
+      -- keymaps
       opts.desc = "Fuzzy find files"
       vim.keymap.set("n", "<C-p>", builtin.find_files, opts)
       opts.desc = "Find recent files"
@@ -42,19 +53,6 @@ return {
       vim.keymap.set("n", "<leader>fs", builtin.live_grep, opts)
       opts.desc = "Find string under cursor"
       vim.keymap.set("n", "<leader>fc", builtin.grep_string, opts)
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-      require("telescope").load_extension("ui-select")
     end,
   },
 }
