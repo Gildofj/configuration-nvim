e043a0d9c2462d00bc1ffc0abeb659ed6dc5d633
Gildo Junior <1gildojunior@gmail.com>
2023-12-29 15:35:00 -0300

    add options and keymaps

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -38,6 +38,12 @@ vim.bo.autoread = true
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
+-- virtual edit in block
+vim.opt.virtualedit = "block"
+
+-- command preview
+vim.opt.inccommand = "split"
+
 -- Yank highlighting
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
