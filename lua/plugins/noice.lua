139379ac06f0776434cc1560dd879cf1641ac038
Gildo Junior <1gildojunior@gmail.com>
2024-01-07 17:32:51 -0300

    ajust healthcheck warns

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,8 +1,18 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+  },
   dependencies = {
     "MunifTanjim/nui.nvim",
   },
+  config = true,
 }
